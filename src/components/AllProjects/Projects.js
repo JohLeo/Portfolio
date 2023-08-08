bee902673439ba252c5fcda5a905b1239854dfda
Johanna Leonsson <johanna@leonsson.se>
2023-08-08 16:47:39 +0200

    new description-texts for projects

--- src/components/AllProjects/Projects.js
+++ src/components/AllProjects/Projects.js
@@ -57,8 +57,8 @@ export const projects = [
     id: '08',
     imgSrc: 'https://i.postimg.cc/yxTWj6Gx/Survey-v6.png',
     title: 'Relaxing survey',
-    description: 'A game made out of Ai lorem ipsum dores lirum A game made out of Ai lorem ipsum dores lirum A game made out of Ai lorem ipsum dores lirum',
-    techs: ['Tech needed'],
+    description: 'A simple survey using controlled form inputs. This project introduced the concept of state in React, and familiarized us with the useState hook and how to show different components depending on one\'s place in a survey. My goal was to create a calm atmosphere, culminating in a soothing breathing exercise for my team amid our studies. ',
+    techs: ['HTML', 'CSS', 'JavaScript', 'React'],
     githubLink: 'https://github.com/JohLeo/project-survey',
     netlifyLink: 'https://stress-less-survey.netlify.app'
   },
@@ -67,35 +67,35 @@ export const projects = [
     imgSrc: 'https://i.postimg.cc/W4QDbHrN/Movies-v8.png',
     title: 'Movie Releases',
     description: 'A multi-page web application using React Router and an API from IMDB. It allows users to view a list of latest movie releases and select a movie to read more about. Built as a pair-programming project.',
-    techs: ['HTML', 'CSS', 'JavaScript', 'React'],
+    techs: ['HTML', 'CSS', 'JavaScript', 'React', 'Mob-programming'],
     githubLink: 'https://github.com/mvfrid/project-movies',
     netlifyLink: 'https://johanna-matilda-project-movies.netlify.app'
   },
   {
     id: '10',
+    imgSrc: 'https://i.postimg.cc/yxwdmm7c/Weather-v3.png',
+    title: 'Weather app',
+    description: 'A basic weather app with a five day forecast for Stockholm. This project focused on integrating APIs and retrieving external data for our website. It involved collaborative mob programming and provided foundational knowledge of working with basic JSON structures, utilizing promises and the fetch() function in JavaScript. Additionally, we gained practical experience in manipulating the Date() object.',
+    techs: ['HTML5', 'CSS3', 'JavaScript', 'Mob-programming'],
+    githubLink: 'https://github.com/sammyolsson/project-weather-app',
+    netlifyLink: 'https://scintillating-tanuki-2a80a2.netlify.app'
+  },
+  {
+    id: '11',
     imgSrc: 'https://i.postimg.cc/vHz6LXRh/Guess-Who-v2.png',
     title: 'Guess what? - game',
-    description: 'A digital version of the old classic game {Guess-Who?}. Using array methods such as filter, forEach and mapping. The project set out to teach us to use JavaScript scope, objects and arrays.',
+    description: 'A digital version of the old classic game "Guess-Who?". Using array methods such as filter, forEach and mapping. The project set out to teach us to use JavaScript scope, objects and arrays.',
     techs: ['HTML', 'CSS', 'JavaScript'],
     githubLink: 'https://github.com/JohLeo/project-guess-who',
     netlifyLink: 'https://poetic-cobbler-eaa36b.netlify.app'
   },
   {
-    id: '11',
+    id: '12',
     imgSrc: 'https://i.postimg.cc/jSSn8Ntb/Chatbot-v1.png',
     title: 'Chatbot',
     description: 'The first JavaScript project in Technigos frontend developer bootcamp. Created to understand the fundamentals of JS. Built by using variables, functions, control flow and conditional statements/ternary operators.',
     techs: ['HTML5', 'CSS3', 'JavaScript'],
     githubLink: 'https://github.com/JohLeo/project-chatbot',
     netlifyLink: 'https://flourishing-begonia-4f3673.netlify.app'
-  },
-  {
-    id: '12',
-    imgSrc: 'https://i.postimg.cc/9FY1wD55/maze-5.png',
-    title: 'Weather app',
-    description: 'Text needed and project is on Sallys Git A game made out of Ai lorem ipsum dores lirum A game made out of Ai lorem ipsum dores lirum A game made out of Ai lorem ipsum dores lirum',
-    techs: ['Tech needed'],
-    githubLink: 'https://github.com/sammyolsson/project-weather-app',
-    netlifyLink: 'https://scintillating-tanuki-2a80a2.netlify.app'
   }
 ];
